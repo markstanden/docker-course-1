07158a452a57ba7f043230001661edba8c6be2ac
Mark Standen <markstanden80@icloud.com>
2020-08-18 20:41:07 +0100

    dockerfile.dev and docker-compose.yml now working to produce a dev server container for the app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Go Nuclear
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          e = mc<sup>2</sup>
         </a>
       </header>
     </div>
